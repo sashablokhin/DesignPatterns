4ca6039c3891c896ce228019cc470f15a680bf86
sashablokhin <sashablokhindev@gmail.com>
2016-04-18 12:27:37 +0300

    Added single responsibility principle

--- DesignPatterns.playground/Contents.swift
+++ DesignPatterns.playground/Contents.swift
@@ -1,5 +1,84 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+/*
+SOLID это аббревиатура пяти основных принципов проектирования классов в объектно-ориентированном программировании — Single responsibility, Open-closed, Liskov substitution, Interface segregation и Dependency inversion.
+*/
+
+/*
+Single responsibility principle - Принцип единственной обязанности
+На каждый класс должна быть возложена одна-единственная обязанность.
+*/
+
+protocol CanBeOn {
+    func on()
+}
+
+protocol CanBeOff {
+    func off()
+}
+
+class Switch: CanBeOff, CanBeOn {
+    private var stateOn = false
+    
+    func on() {
+        stateOn = true
+    }
+    
+    func off() {
+        stateOn = false
+    }
+}
+
+class SwitchOn {
+    let switcher: CanBeOn
+    
+    init(switcher: CanBeOn) {
+        self.switcher = switcher
+    }
+    
+    func execute() {
+        switcher.on()
+    }
+}
+
+class SwitchOff {
+    let switcher: CanBeOff
+    
+    init(switcher: CanBeOff) {
+        self.switcher = switcher
+    }
+    
+    func execute() {
+        switcher.off()
+    }
+}
+
+let switcher = Switch()
+switcher.stateOn
+
+let switchOn = SwitchOn(switcher: switcher)
+switchOn.execute()
+
+switcher.stateOn
+
+
+/*
+Open/closed principle - Принцип открытости/закрытости
+Программные сущности должны быть открыты для расширения, но закрыты для изменения.
+*/
+
+/*
+Liskov substitution principle - Принцип подстановки Барбары Лисков
+Функции, которые используют базовый тип, должны иметь возможность использовать подтипы базового типа, не зная об этом.
+Подклассы не могут замещать поведения базовых классов. Подтипы должны дополнять базовые типы.
+*/
+
+/*
+Interface segregation principle - Принцип разделения интерфейса
+Много специализированных интерфейсов лучше, чем один универсальный.
+*/
+
+/*
+Dependency inversion principle - Принцип инверсии зависимостей
+Зависимости внутри системы строятся на основе абстракций. Модули верхнего уровня не зависят от модулей нижнего уровня. Абстракции не должны зависеть от деталей. Детали должны зависеть от абстракций.
+*/

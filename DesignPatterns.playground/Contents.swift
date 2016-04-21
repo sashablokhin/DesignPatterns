76823b4c03ee65ecb81fc10a65c2c6bca8c5500d
sashablokhin <sashablokhindev@gmail.com>
2016-04-21 18:07:24 +0300

    Added interpreter pattern

--- DesignPatterns.playground/Contents.swift
+++ DesignPatterns.playground/Contents.swift
@@ -857,8 +857,92 @@ tv.off()
 
 
 /*
-Интерпретатор (англ. Interpreter) — поведенческий шаблон проектирования, решающий часто встречающуюся, но подверженную изменениям, задачу.
+Интерпретатор (англ. Interpreter) — поведенческий шаблон проектирования, решающий часто встречающуюся, но подверженную изменениям, задачу. Определяет грамматическое представление для языка и интерпретатор для интерпретации грамматики.
 */
+
+// Интерпретатор
+protocol IntegerExp {
+    func evaluate(context: IntegerContext) -> Int
+    func replace(character: Character, integerExp: IntegerExp) -> IntegerExp
+    func copy() -> IntegerExp
+}
+
+// Контекст
+class IntegerContext {
+    private var data: [Character: Int] = [:]
+    
+    func lookup(name: Character) -> Int {
+        return self.data[name]!
+    }
+    
+    func assign(integerVarExp: IntegerVarExp, value: Int) {
+        self.data[integerVarExp.name] = value
+    }
+}
+
+// Переменные
+class IntegerVarExp: IntegerExp {
+    let name: Character
+    
+    init(name: Character) {
+        self.name = name
+    }
+    
+    func evaluate(context: IntegerContext) -> Int {
+        return context.lookup(self.name)
+    }
+    
+    func replace(character: Character, integerExp: IntegerExp) -> IntegerExp {
+        if name == self.name {
+            return integerExp.copy()
+        } else {
+            return IntegerVarExp(name: self.name)
+        }
+    }
+    
+    func copy() -> IntegerExp {
+        return IntegerVarExp(name: self.name)
+    }
+}
+
+// Грамматика
+class AddExp: IntegerExp {
+    private var op1: IntegerExp
+    private var op2: IntegerExp
+    
+    init(op1: IntegerExp, op2: IntegerExp) {
+        self.op1 = op1
+        self.op2 = op2
+    }
+    
+    func evaluate(context: IntegerContext) -> Int {
+        return self.op1.evaluate(context) + self.op2.evaluate(context)
+    }
+    
+    func replace(character: Character, integerExp: IntegerExp) -> IntegerExp {
+        return AddExp(op1: op1.replace(character, integerExp: integerExp), op2: op2.replace(character, integerExp: integerExp))
+    }
+    
+    func copy() -> IntegerExp {
+        return AddExp(op1: self.op1, op2: self.op2)
+    }
+}
+
+var expression: IntegerExp?
+var integerContext = IntegerContext()
+
+var a = IntegerVarExp(name: "a")
+var b = IntegerVarExp(name: "b")
+var c = IntegerVarExp(name: "c")
+
+expression = AddExp(op1: a, op2: AddExp(op1: b, op2: c)) // a + (b + c)
+
+integerContext.assign(a, value: 2)
+integerContext.assign(b, value: 6)
+integerContext.assign(c, value: 3)
+
+var result = expression?.evaluate(integerContext)
+
 
 /*
 Итератор (англ. Iterator) — поведенческий шаблон проектирования. Представляет собой объект, позволяющий получить последовательный доступ к элементам объекта-агрегата без использования описаний каждого из агрегированных объектов.

e12c2d46bfb5b05df9b60b8f0ba81dcb141cd8f8
sashablokhin <sashablokhindev@gmail.com>
2016-04-25 17:24:17 +0300

    Add mediator pattern

--- DesignPatterns.playground/Contents.swift
+++ DesignPatterns.playground/Contents.swift
@@ -991,6 +991,70 @@ for drink in drinksAutomate {
 Посредник (англ. Mediator) — поведенческий шаблон проектирования, обеспечивающий взаимодействие множества объектов, формируя при этом слабую связанность и избавляя объекты от необходимости явно ссылаться друг на друга.
 */
 
+class Sensor {
+    let mediator: Mediator
+    
+    init(mediator: Mediator) {
+        self.mediator = mediator
+    }
+    
+    func send(message: String) {
+        mediator.send(message, sensor: self)
+    }
+    
+    func receive(message: String, sensor: Sensor) {
+        
+    }
+}
+
+protocol Mediator {
+    func send(message: String, sensor: Sensor)
+}
+
+class Computer: Mediator {
+    private var sensors = [Sensor]()
+    
+    func addSensor(sensor: Sensor) {
+        sensors.append(sensor)
+    }
+    
+    func send(message: String, sensor: Sensor) {
+        for s in sensors {
+            if s !== sensor {
+                sensor.receive(message, sensor: s)
+            }
+        }
+    }
+}
+
+class ConcreteSensor: Sensor {
+    let name: String
+
+    init(name: String, mediator: Mediator) {
+        self.name = name
+        super.init(mediator: mediator)
+    }
+    
+    override func receive(message: String, sensor: Sensor) {
+        guard let sensor = sensor as? ConcreteSensor else {return}
+        
+        print("Sensor \(sensor.name) received: \(message)")
+    }
+}
+
+let computer = Computer()
+
+let camera = ConcreteSensor(name: "camera", mediator: computer)
+let radar = ConcreteSensor(name: "radar", mediator: computer)
+let gps = ConcreteSensor(name: "gps", mediator: computer)
+
+computer.addSensor(camera)
+computer.addSensor(radar)
+computer.addSensor(gps)
+
+gps.send("lat: 20.34, lon: 50.23")
+
+
 /*
 Хранитель (англ. Memento) — поведенческий шаблон проектирования, позволяющий, не нарушая инкапсуляцию, зафиксировать и сохранить внутреннее состояние объекта так, чтобы позднее восстановить его в это состояние.
 */
